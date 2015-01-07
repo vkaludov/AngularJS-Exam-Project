1cf1cb5db8b5581cff096b07da9c54737be7ed80
vkaludov <vasil.kaludov@abv.bg>
2015-01-08 01:53:09 +0200

    login modified

--- Ads-Exam-Project/js/controllers/LoginController.js
+++ Ads-Exam-Project/js/controllers/LoginController.js
@@ -1,9 +1,13 @@
 'use strict';
 
-app.controller('LoginController', ['$scope', 'userData', function($scope, userData) {
+app.controller('LoginController', ['$scope', '$location', 'userData', function($scope, $location, userData) {
     $scope.pageTitle = 'Login';
 
     $scope.login = function (user) {
-        userData.login(user);
+        userData.login(user)
+            .$promise
+            .then(function(data) {
+               $location.path('/');
+            });
     }
 }]);

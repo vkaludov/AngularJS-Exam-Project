d4714221c26bc837f12e3e300d3df2a8a361b2db
vkaludov <vasil.kaludov@abv.bg>
2015-01-07 21:17:30 +0200

    added functionality to register form

--- Ads-Exam-Project/js/controllers/RegisterController.js
+++ Ads-Exam-Project/js/controllers/RegisterController.js
@@ -1,10 +1,15 @@
 'use strict';
 
 app.controller('RegisterController', ['$scope', 'townsData', function($scope, townsData) {
+    $scope.pageTitle = 'Registration';
+    
     townsData.getTowns()
         .$promise
         .then(function(data) {
             $scope.towns = data;
         });
-    $scope.pageTitle = 'Registration';
+    
+    $scope.register = function (user) {
+        //userData.register(user);
+    }
 }]);

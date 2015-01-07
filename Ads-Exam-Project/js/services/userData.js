eb385d52ae8e289bed43b32711909575e1a775ff
vkaludov <vasil.kaludov@abv.bg>
2015-01-07 23:10:12 +0200

    added login and logout to authentication.js

--- Ads-Exam-Project/js/services/userData.js
+++ Ads-Exam-Project/js/services/userData.js
@@ -1,9 +1,12 @@
 app.factory('userData', ['$resource', 'baseServiceUrl', 'authentication', function($resource, baseServiceUrl, authentication) {
 
     function userLogin(user) {
-        //return $resource(baseServiceUrl + 'user/login')
-        //    .save(user);
-        //authentication.saveUser(token);
+        return $resource(baseServiceUrl + 'user/login')
+            .save(user)
+            .$promise
+            .then(function(data) {
+                authentication.saveUser(data);
+        });
     }
 
     function userRegister(user) {
@@ -12,11 +15,16 @@ app.factory('userData', ['$resource', 'baseServiceUrl', 'authentication', functi
             .$promise
             .then(function(data) {
                 authentication.saveUser(data);
-            });
+        });
     }
 
     function userLogout(user) {
-        return resource.query();
+        return $resource(baseServiceUrl + 'user/logout')
+            .save(user)
+            .$promise
+            .then(function() {
+                authentication.removeUser();
+            });
     }
 
     return{

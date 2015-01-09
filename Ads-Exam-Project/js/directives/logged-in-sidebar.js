2da57ba04abbbe1b906061c930bd271445479d46
vkaludov <vasil.kaludov@abv.bg>
2015-01-09 20:23:25 +0200

    logged-in-sidebar.html moved to user folder

--- Ads-Exam-Project/js/directives/logged-in-sidebar.js
+++ Ads-Exam-Project/js/directives/logged-in-sidebar.js
@@ -2,7 +2,7 @@ app.directive('loggedInSidebar', function() {
     return {
         controller: 'HomeController',
         restrict: 'E',
-        templateUrl: 'templates/directives/logged-in-sidebar.html',
+        templateUrl: 'templates/user/logged-in-sidebar.html',
         replace: true
     };
 });

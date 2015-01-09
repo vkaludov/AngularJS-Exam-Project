f48c94275afa542d3475faa4fdcde2a1a66d1d5a
vkaludov <vasil.kaludov@abv.bg>
2015-01-09 22:24:57 +0200

    added towns and categories

--- Ads-Exam-Project/js/controllers/PublishNewAdController.js
+++ Ads-Exam-Project/js/controllers/PublishNewAdController.js
@@ -1,9 +1,15 @@
 'use strict';
 
-app.controller('PublishNewAdController', ['$scope', 'categoriesData', function($scope, categoriesData) {
+app.controller('PublishNewAdController', ['$scope', 'categoriesData', 'townsData', function($scope, categoriesData, townsData) {
     categoriesData.getCategories()
         .$promise
         .then(function(data) {
             $scope.categories = data;
+        });
+
+    townsData.getTowns()
+        .$promise
+        .then(function(data) {
+            $scope.towns = data;
         });
 }]);

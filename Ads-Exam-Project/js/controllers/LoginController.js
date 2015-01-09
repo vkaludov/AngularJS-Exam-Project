cc6e4544b9630acd400a67aef57d3a90edde38b5
vkaludov <vasil.kaludov@abv.bg>
2015-01-10 00:55:04 +0200

    added notifications for login

--- Ads-Exam-Project/js/controllers/LoginController.js
+++ Ads-Exam-Project/js/controllers/LoginController.js
@@ -1,11 +1,14 @@
 'use strict';
 
-app.controller('LoginController', ['$scope', '$location', 'userData', function($scope, $location, userData) {
+app.controller('LoginController', ['$scope', '$location', 'userData', 'notifyService', function($scope, $location, userData, notifyService) {
     $scope.login = function (user) {
         userData.login(user)
             .$promise
             .then(function() {
+                notifyService.showInfo("Login successful.");
                 $location.path('/user/home');
+            }, function (serverError) {
+                notifyService.showError("Login failed:", serverError);
             });
     }
 }]);

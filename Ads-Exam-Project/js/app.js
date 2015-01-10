aa8471f0b8566e2527e841495979b7287760172a
vkaludov <vasil.kaludov@abv.bg>
2015-01-10 12:50:21 +0200

    bootstrap pagination added

--- Ads-Exam-Project/js/app.js
+++ Ads-Exam-Project/js/app.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var app = angular.module('softUniProject', ['ngRoute', 'ngResource', 'LocalStorageModule']);
+var app = angular.module('softUniProject', ['ngRoute', 'ngResource', 'LocalStorageModule', 'ui.bootstrap.pagination']);
 
 app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net/api/');
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
+        templateUrl: 'template/home.html',
         controller: 'HomeController'
     });
     $routeProvider.when('/login', {
-        templateUrl: 'templates/login.html',
+        templateUrl: 'template/login.html',
         controller: 'LoginController'
     });
     $routeProvider.when('/register', {
-        templateUrl: 'templates/register.html',
+        templateUrl: 'template/register.html',
         controller: 'RegisterController'
     });
     $routeProvider.when('/logout', {
-        templateUrl: 'templates/home.html',
+        templateUrl: 'template/home.html',
         controller: 'HomeController'
     });
     $routeProvider.when('/user/home', {
-        templateUrl: 'templates/home.html',
+        templateUrl: 'template/home.html',
         controller: 'HomeController'
     });
     $routeProvider.when('/user/ads', {
-        templateUrl: 'templates/user/publish-new-ad.html',
+        templateUrl: 'template/user/publish-new-ad.html',
         controller: 'ListUserAdsController'
     });
     $routeProvider.when('/user/ads/publish', {
-        templateUrl: 'templates/user/publish-new-ad.html',
+        templateUrl: 'template/user/publish-new-ad.html',
         controller: 'PublishNewAdController'
     });
     $routeProvider.otherwise({
